2654d59f9d52b203f5cc72128deeab80b6fad7e5
enjellinabely <157446887+enjellinabely@users.noreply.github.com>
2024-01-29 15:04:16 +0700

    Add files via upload

---
+++ daftar.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body{
+    background-image:url('back.jpg');
+    background-size: cover;
+    background-position: center;
+    width: auto;
+    height: auto
+}
+.main{
+    margin: auto;
+    width: 800px;
+    padding:10px;
+    background: white;
+}
+.card{
+    display: flex;
+    text-align: center;
+}
+.card1{
+    display: flex;
+    text-align: center;
+}
+.card2{
+    display: flex;
+    text-align: center;
+}
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buku</title>
+</head>
+<body>
+<img src="Logo SMK MTS.png" width="80px" height="80px" align="left"/>
+<h1 align="center">Perpustakaan Digital</h1>
+<br>
+<div class="main">
+<h2 align="center" >Daftar Buku</h2>
+<br>
+<div class="card">
+<tr>
+<td style="padding:5px 10px;">
+<img src="bukucss.jpeg" style="height:200px;"/>
+<p>Judul: CSS Uncover<br>
+Karya: Andre Pratama</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="boostrap.jpeg" style="height:200px;"/>
+<p>Judul: Bootstrap Uncover<br>
+Karya: Andre Pratama</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="html.jpeg" style="height:200px;"/>
+<p>Judul: HTML Uncover<br>
+Karya: Andre Pratama</p>
+</td>
+</tr>
+</div>
+<br>
+<br>
+<div class="card1">
+<tr>
+<td style="padding:5px 10px;">
+<img src="kundang.jpg" style="height:200px;"/>
+<p>Judul: Malin Kundang<br>
+Karya: Yustitia Angelia</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="alaska.jpeg" style="height:200px;"/>
+<p>Judul: Teluk Alaska<br>
+Karya: Eka Aryani</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="dilan.jpeg" style="height:200px;"/>
+<p>Judul: Dilan<br>
+Karya: Pidi Baiq</p>
+</td>
+</tr>
+</div>
+<br>
+<br>
+<div class="card2">
+<tr>
+<td style="padding:5px 10px;">
+<img src="ayah.jpeg" style="height:200px;"/>
+<p>Judul: Ayah<br>
+Karya: Andrea Hirata</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="sejarah.jpeg" style="height:200px;"/>
+<p>Judul: Sejarah Pendidikan Nasional<br>
+Karya: Muhammad Rifa'i</p>
+</td>
+</tr>
+</div>
+</body>
+</html>
---
+++ peminjam.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body{
+    padding-top: 80px; 
+}
+.alert{
+    font-weight: bold;
+    text-align: left;
+}
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peminjam - Aplikasi Perpustakaan Digital</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container">
+    <h3> Aplikasi Perpustakaan Digital</h3>
+    <div class="alert alert-secondary" class="alert">
+    Anda login sebagai <b>PEMINJAM</b> Aplikasi Perpustakaan Digital
+    </div>
+    <button type="button" class="btn btn-primary">Peminjam</button>
+    <button type="button" class="btn btn-primary">Daftar Buku</button>
+    <button type="button" class="btn btn-primary">Tanggal Peminjaman</button>
+    <button type="button" class="btn btn-primary">Tanggal Pengembalian</button>
+    <button type="button" class="btn btn-primary">Logout</button>
+    <br>
+    <br>
+    <div class="card">
+    <h4> Selamat Datang Di Halaman Peminjam</h4>
+    <p> Aplikasi Perpustakaan Digital digunakan untuk mempermudah dalam melakukakan peminjaman buku</p>
+    </div>
+    </div>
+</body>
+</html>
